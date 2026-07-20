9d0b75a68e2a5841a1cd3983cc231e2de066a88b
vkfullstack <vkhetre69@gmail.com>
2026-07-20 10:29:08 +0530

    feat(app): add Spring Boot application entry point

---
+++ Hontec-Backend-Assessment1/src/main/java/com/Hontec/HontecBackendAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.Hontec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HontecBackendAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HontecBackendAssessmentApplication.class, args);
+	}
+
+}
